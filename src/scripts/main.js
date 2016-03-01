cd3b173606cf9205f17be5944858c35fcbbc465c
Tobias Reich <tobias.reich.ich@gmail.com>
2016-03-01 13:32:51 +0100

    Return true after show/hide

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -123,6 +123,8 @@ export const show = function(html, opts) {
 		})
 	}, 10)
 
+	return true
+
 }
 
 export const close = function(opts) {
@@ -157,6 +159,8 @@ export const close = function(opts) {
 
 		})
 	}, 410)
+
+	return true
 
 }
 
